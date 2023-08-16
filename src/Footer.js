bcb3bfdfbb19112c0c2c60f94e497a29fe223f57
Green Gem <chris_waitt@me.com>
2023-08-16 13:51:09 +0100

    Made footer full-width using bootrap container-fluid

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/esm/Container';
 
 function AppFooter() {
   return (
-<Container>
+<Container fluid>
   <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
     <p className="col-md-4 mb-0 text-muted">© 2022 Company, Inc</p>
 
