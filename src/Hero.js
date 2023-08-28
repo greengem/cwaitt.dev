813c64a42912bf4a6d5a14eadf17f4f147bcdf2d
Green Gem <chris_waitt@me.com>
2023-08-28 15:39:33 +0100

    feat: Setting up for Hero section at top of homepage, using random images for now.

---
+++ src/Hero.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import './Hero.css';
+import { Github, Linkedin } from 'react-bootstrap-icons';
+
+function AppHero() {
+  return (
+    <section className='hero'>
+      <Container fluid>
+        <div className="overlay"></div> {/* Overlay for darkened effect */}
+        <div className="hero-content">
+          <Container>
+            <h1>Hi, I'm Chris Waitt</h1>
+            <h2>Frontend Developer / UIUX Designer</h2>
+            <h1>
+              <span className='me-4'><a href='https://github.com/greengem'><Github /></a></span>
+              <span><a href='https://www.linkedin.com/in/chriswaitt/'><Linkedin /></a></span>
+            </h1>
+          </Container>
+        </div>
+      </Container>
+    </section>
+  );
+}
+
+export default AppHero;
