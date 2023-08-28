cd09a79375ff5c963e8055059f2eb4737acffdd1
Green Gem <chris_waitt@me.com>
2023-08-28 15:37:37 +0100

    feat: Added dark mode switch to Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,31 +1,52 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar'; // Import the Navbar component from React Bootstrap
-import Nav from 'react-bootstrap/Nav'; // Import the Nav component from React Bootstrap
-import Container from 'react-bootstrap/Container'; // Import the Container component from React Bootstrap
-/*import { Github, Linkedin, Envelope } from 'react-bootstrap-icons';*/
+import React, { useState, useEffect } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import { MoonStarsFill, SunFill } from 'react-bootstrap-icons';
+import Form from 'react-bootstrap/Form';
 
 function AppNavbar() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    // Check local storage for user preference
+    const savedDarkMode = localStorage.getItem('darkMode') === 'enabled';
+    setDarkMode(savedDarkMode);
+  }, []);
+
+useEffect(() => {
+  // Update data-bs-theme attribute on <html> element
+  if (darkMode) {
+    document.documentElement.setAttribute('data-bs-theme', 'dark');
+    localStorage.setItem('darkMode', 'enabled');
+  } else {
+    document.documentElement.setAttribute('data-bs-theme', 'light');
+    localStorage.setItem('darkMode', 'disabled');
+  }
+}, [darkMode]);
+
+
   return (
-    // Create a responsive navigation bar with a dark background
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar className='navbar sticky-top bg-body-tertiary'>
       <Container>
-        {/* Display your brand logo or name */}
         <Navbar.Brand href="/">Site Title</Navbar.Brand>
-        
-        {/* Toggle button for collapsing the navigation menu on smaller screens */}
         <Navbar.Toggle aria-controls="navbar-nav" />
-        
-        {/* Collapsible navigation menu */}
         <Navbar.Collapse id="navbar-nav">
           <Nav className="ml-auto">
-            {/* Navigation links */}
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#">About</Nav.Link>
-            <Nav.Link href="#">Contact</Nav.Link>
             <Nav.Link href="https://www.linkedin.com/in/chriswaitt/" target='_blank' rel='noopener noreferrer'>Linkedin</Nav.Link>
             <Nav.Link href="https://github.com/greengem" target='_blank' rel='noopener noreferrer'>Github</Nav.Link>
           </Nav>
         </Navbar.Collapse>
+        <SunFill className='me-2' />
+        
+        <Form.Check
+          type="switch"
+          id="darkmode-switch"
+          checked={darkMode}
+          onChange={() => setDarkMode(!darkMode)}
+        />
+        <MoonStarsFill />
       </Container>
     </Navbar>
   );
