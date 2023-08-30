25683a4c40e08e781bbc8eccad22a6df423e0e0f
Green Gem <chris_waitt@me.com>
2023-08-30 10:58:10 +0100

    Added animate.css and react-animation-on-scroll to implement scroll animations

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "animate.css/animate.min.css";
 /*import 'bootswatch/dist/cosmo/bootstrap.min.css';*/
 import './index.css';
 
