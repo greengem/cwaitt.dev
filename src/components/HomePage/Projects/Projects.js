f84745c60966764c41585a44e4276360f0c9862b
Green Gem <chris_waitt@me.com>
2023-08-30 11:00:22 +0100

    Rolled back full image cards to original state as text was not readable. added animation to title

--- src/components/HomePage/Projects/Projects.js
+++ src/components/HomePage/Projects/Projects.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
 import * as contentful from 'contentful';
-
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
 import BadgeComponent from '../../Common/BadgeComponent/BadgeComponent';
-
 import './Projects.css';
 
 const client = contentful.createClient({
@@ -36,16 +34,19 @@ function Projects() {
 
   return (
     <section className='section-portfolio'>
-    <Container className="mt-5">
-      <h1>Portfolio</h1>
-      <Row xs={1} sm={1} md={2} lg={3} className="g-4">
-        {projects.map((project) => (
-          <Col key={project.sys.id}>
-            <Card className="h-100 custom-card">
+      <Container className="mt-5">
+        <AnimationOnScroll animateOnce={true} animateIn="animate__fadeInRightBig">
+          <h1>Projects</h1>
+        </AnimationOnScroll>
+        <Row xs={1} sm={1} md={2} lg={2} className="g-4">
+          {projects.map((project) => (
+            <Col key={project.sys.id}>
+              <Card className="h-100 custom-card">
+                <Link to={`/projects/${project.sys.id}`}>
                   {project.fields.thumbnailImage ? (
                     <Card.Img
                       variant="top"
-                      src={project.fields.thumbnailImage.fields.file.url}
+                      src={`${project.fields.thumbnailImage.fields.file.url}?fit=fill&w=600&h=400`}
                       alt={project.fields.projectTitle}
                     />
                   ) : (
@@ -55,27 +56,23 @@ function Projects() {
                       alt="Placeholder"
                     />
                   )}
-              <Card.ImgOverlay>
-                <Card.Title>
-                <Link to={`/projects/${project.sys.id}`}>
-                  {project.fields.projectTitle}
-                  </Link>
-                  </Card.Title>
-                <Card.Text>
-                  {/*{project.fields.shortDescription}
-                  </Card.Text>*/}
+                </Link>
+                <Card.Body>
+                  <Card.Title>{project.fields.projectTitle}</Card.Title>
+                  <Card.Text>{project.fields.shortDescription}</Card.Text>
+                </Card.Body>
+                <Card.Footer>
                   <div className='tech-stack-labels'>
-                  {project.fields.techStack.map((tech, index) => (
-                    <BadgeComponent key={index} tech={tech} />
-                  ))}
+                    {project.fields.techStack.map((tech, index) => (
+                      <BadgeComponent key={index} tech={tech} />
+                    ))}
                   </div>
-                </Card.Text>
-              </Card.ImgOverlay>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Container>
+                </Card.Footer>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </section>
   );
 }
