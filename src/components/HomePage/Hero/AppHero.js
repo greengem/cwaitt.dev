2706a208d2a4a98630ef1325c781b3fb206323b4
Green Gem <chris_waitt@me.com>
2023-08-30 10:59:04 +0100

    design: Removed social icons from Hero box

--- src/components/HomePage/Hero/AppHero.js
+++ src/components/HomePage/Hero/AppHero.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import './Hero.css';
-import { Github, Linkedin } from 'react-bootstrap-icons';
 
 function AppHero() {
   return (
@@ -11,10 +10,6 @@ function AppHero() {
         <Container>
           <h1>Hi, I'm Chris Waitt</h1>
           <h2>Frontend Developer / Designer</h2>
-          <h1>
-            <span className='me-4'><a href='https://github.com/greengem'><Github /></a></span>
-            <span><a href='https://www.linkedin.com/in/chriswaitt/'><Linkedin /></a></span>
-          </h1>
         </Container>
       </div>
     </section>
