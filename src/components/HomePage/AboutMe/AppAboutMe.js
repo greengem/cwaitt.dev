5118365bee4426d4123b0fcd823915aa465d4cac
Green Gem <chris_waitt@me.com>
2023-08-30 11:01:38 +0100

    Added scroll animations to about me section.

--- src/components/HomePage/AboutMe/AppAboutMe.js
+++ src/components/HomePage/AboutMe/AppAboutMe.js
@@ -5,24 +5,34 @@ import Row from 'react-bootstrap/Row';
 import './AboutMe.css';
 import SpotifyPlaylist from './Spotify/SpotifyPlaylist';
 import { SpeakerFill } from 'react-bootstrap-icons';
-
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 function AppAboutMe() {
   return (
     <section className='about-me'>
       <Row xs={1} sm={1} md={2} lg={2}>
-        <Col className='about-me-photo' ></Col>
+        <Col className='about-me-photo'></Col>
         <Col className='about-me-text'>
           <Container>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <hr />
-            <p><SpeakerFill /> What i'm listening to</p>
+            <AnimationOnScroll animateOnce={true} animateIn="animate__fadeInLeftBig">
+              <h1>About</h1>
+            </AnimationOnScroll>
+            <AnimationOnScroll animateOnce={true} delay={500} animateIn="animate__fadeInLeftBig">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </AnimationOnScroll>
+            <AnimationOnScroll animateOnce={true} delay={1000} animateIn="animate__fadeInLeftBig">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </AnimationOnScroll>
+            <AnimationOnScroll animateOnce={true} delay={1500} animateIn="animate__fadeInLeftBig">
               <SpotifyPlaylist />
+            </AnimationOnScroll>
           </Container>
         </Col>
       </Row>
-
     </section>
   );
 }
