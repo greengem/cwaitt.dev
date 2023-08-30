843b9481fbbb25431901cfc2eb43ded577b8f964
Green Gem <chris_waitt@me.com>
2023-08-30 13:18:50 +0100

    Added about me text with scroll animations

--- src/components/HomePage/AboutMe/AppAboutMe.js
+++ src/components/HomePage/AboutMe/AppAboutMe.js
@@ -4,7 +4,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import './AboutMe.css';
 import SpotifyPlaylist from './Spotify/SpotifyPlaylist';
-import { SpeakerFill } from 'react-bootstrap-icons';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 function AppAboutMe() {
@@ -17,16 +16,47 @@ function AppAboutMe() {
             <AnimationOnScroll animateOnce={true} animateIn="animate__fadeInLeftBig">
               <h1>About</h1>
             </AnimationOnScroll>
-            <AnimationOnScroll animateOnce={true} delay={500} animateIn="animate__fadeInLeftBig">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
+
+            <AnimationOnScroll animateOnce={true} animateIn="animate__fadeInLeftBig">
+              <p>Hey there! I'm <strong>Chris</strong>, a passionate <strong>web dev</strong> with a deep love for all things tech. I thrive on turning innovative ideas into reality and creating solutions that make a meaningful impact. 🚀</p>
             </AnimationOnScroll>
-            <AnimationOnScroll animateOnce={true} delay={1000} animateIn="animate__fadeInLeftBig">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
+
+            <AnimationOnScroll animateOnce={true} animateIn="animate__fadeInLeftBig">
+              <p>My journey into the world of development began when I was running a record label...</p>
             </AnimationOnScroll>
+
+            <AnimationOnScroll animateOnce={true} animateIn="animate__fadeInLeftBig">
+              <p>I started by teaching myself the basics of <strong>HTML</strong> and <strong>CSS</strong>, piecing together my first websites...</p>
+            </AnimationOnScroll>
+
+            <AnimationOnScroll animateOnce={true} animateIn="animate__fadeInLeftBig">
+              <p>As my ambitions grew, so did my skills. I delved into using <strong>PHP</strong> for includes and custom functions...</p>
+            </AnimationOnScroll>
+
+            <AnimationOnScroll animateOnce={true} animateIn="animate__fadeInLeftBig">
+              <p>After getting acquainted with content management systems (<strong>CMS</strong>), I discovered the unbelievable power of <strong>JavaScript</strong>...</p>
+            </AnimationOnScroll>
+
+            <AnimationOnScroll animateOnce={true} animateIn="animate__fadeInLeftBig">
+              <p>I specialize in <strong>frontend development</strong>, <strong>responsive design</strong>, <strong>web accessibility</strong>, and <strong>problem-solving</strong>, where I combine my technical skills with creative problem-solving to build robust and user-friendly applications. Whether it's crafting elegant frontend interfaces, ensuring seamless responsiveness, adhering to accessibility standards, or architecting scalable backend systems, I'm up for the challenge. 🎨</p>
+            </AnimationOnScroll>
+
+            <AnimationOnScroll animateOnce={true} animateIn="animate__fadeInLeftBig">
+              <p>I believe that great software is not just lines of code; it's a harmonious blend of innovation, usability, and reliability. I'm dedicated to writing clean, maintainable code that not only meets functional requirements but also sets the foundation for future growth. 🔧</p>
+            </AnimationOnScroll>
+
+            <AnimationOnScroll animateOnce={true} animateIn="animate__fadeInLeftBig">
+              <p>In the world of technology, learning never stops. I'm a firm believer in staying up-to-date with the latest trends and tools in the industry. You'll often find me diving into new frameworks, attending tech meetups, and exploring online courses. 📚</p>
+            </AnimationOnScroll>
+
+            <AnimationOnScroll animateOnce={true} animateIn="animate__fadeInLeftBig">
+              <p>When I'm not immersed in code, you'll find me <em>cycling along the picturesque London canals, enjoying aquatic activities (though definitely not swimming in the canals), diving into creative music composition, and indulging in PC gaming</em>. These diverse interests contribute to my well-rounded perspective and help me approach challenges from refreshingly different angles. 🚴‍♂️🎶🎮</p>
+            </AnimationOnScroll>
+
+            <AnimationOnScroll animateOnce={true} animateIn="animate__fadeInLeftBig">
+              <p>I'm always excited to collaborate, share ideas, and contribute to exciting projects. Whether you're a fellow developer, a potential client, or just someone passionate about technology, I'd love to connect! 🤝</p>
+            </AnimationOnScroll>
+
             <AnimationOnScroll animateOnce={true} delay={1500} animateIn="animate__fadeInLeftBig">
               <SpotifyPlaylist />
             </AnimationOnScroll>
