a6d2a522d0adc62f7d030663c95ea8d94165ec2a
Green Gem <chris_waitt@me.com>
2023-08-31 20:10:05 +0100

    Removed unused import

--- src/components/HomePage/AboutMe/AppAboutMe.js
+++ src/components/HomePage/AboutMe/AppAboutMe.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Accordion from 'react-bootstrap/Accordion';
 import './AboutMe.css';
 import SpotifyPlaylist from './Spotify/SpotifyPlaylist';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
