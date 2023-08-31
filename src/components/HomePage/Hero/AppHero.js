d1ca8fad809b0c46c0c1ee7a612e347eab6ca7c9
Green Gem <chris_waitt@me.com>
2023-08-31 09:06:10 +0100

    Experimental CSS to make Hero text have a glitching erffect

--- src/components/HomePage/Hero/AppHero.js
+++ src/components/HomePage/Hero/AppHero.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import './Hero.css';
+import './HeroGlitch.css';
 
 function AppHero() {
   return (
-    <section className='hero'>
-      <div className="hero-overlay"></div> {/* Overlay for darkened effect */}
+    <section className='hero-container'>
+      <div className="hero-overlay"></div>
       <div className="hero-content">
         <Container>
-          <h1>Hi, I'm Chris Waitt</h1>
-          <h2>Frontend Developer / Designer</h2>
+          <h1 className="hero glitch layers" data-text="Hi, I'm Chris Waitt"><span>Hi, I'm Chris Waitt</span></h1>
+          <h2>Architecting Futuristic Web Experiences</h2>
         </Container>
       </div>
     </section>
