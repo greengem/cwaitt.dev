8854fb782b4f9e94aa91aa58db0d94bd0b8dab18
Green Gem <chris_waitt@me.com>
2023-09-04 14:11:43 +0100

    Made every other project card animate from left or right.

--- src/components/HomePage/Projects/Projects.js
+++ src/components/HomePage/Projects/Projects.js
@@ -9,15 +9,18 @@ import Card from 'react-bootstrap/Card';
 import BadgeComponent from '../../Common/BadgeComponent/BadgeComponent';
 import './Projects.css';
 
+// Create a Contentful client
 const client = contentful.createClient({
   space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
   accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
 });
 
 function Projects() {
+  // State to hold the fetched projects
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
+    // Fetch projects from Contentful when the component mounts
     async function fetchProjects() {
       try {
         const response = await client.getEntries({
@@ -39,36 +42,41 @@ function Projects() {
           <h1>Projects</h1>
         </AnimationOnScroll>
         <Row xs={1} sm={1} md={2} lg={2} className="g-4">
-          {projects.map((project) => (
+          {projects.map((project, index) => (
             <Col key={project.sys.id}>
-              <Card className="h-100 custom-card">
-                <Link to={`/projects/${project.sys.id}`}>
-                  {project.fields.thumbnailImage ? (
-                    <Card.Img
-                      variant="top"
-                      src={`${project.fields.thumbnailImage.fields.file.url}?fit=fill&w=600&h=400`}
-                      alt={project.fields.projectTitle}
-                    />
-                  ) : (
-                    <Card.Img
-                      variant="top"
-                      src="https://placehold.co/600x400"
-                      alt="Placeholder"
-                    />
-                  )}
-                </Link>
-                <Card.Body>
-                  <Card.Title>{project.fields.projectTitle}</Card.Title>
-                  <Card.Text>{project.fields.shortDescription}</Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                  <div className='tech-stack-labels'>
-                    {project.fields.techStack.map((tech, index) => (
-                      <BadgeComponent key={index} tech={tech} />
-                    ))}
-                  </div>
-                </Card.Footer>
-              </Card>
+              <AnimationOnScroll
+                animateOnce={true}
+                animateIn={index % 2 === 0 ? "animate__fadeInLeftBig" : "animate__fadeInRightBig"}
+              >
+                <Card className="h-100 custom-card">
+                  <Link to={`/projects/${project.sys.id}`}>
+                    {project.fields.thumbnailImage ? (
+                      <Card.Img
+                        variant="top"
+                        src={`${project.fields.thumbnailImage.fields.file.url}?fit=fill&w=600&h=400`}
+                        alt={project.fields.projectTitle}
+                      />
+                    ) : (
+                      <Card.Img
+                        variant="top"
+                        src="https://placehold.co/600x400"
+                        alt="Placeholder"
+                      />
+                    )}
+                  </Link>
+                  <Card.Body>
+                    <Card.Title>{project.fields.projectTitle}</Card.Title>
+                    <Card.Text>{project.fields.shortDescription}</Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <div className='tech-stack-labels'>
+                      {project.fields.techStack.map((tech, index) => (
+                        <BadgeComponent key={index} tech={tech} />
+                      ))}
+                    </div>
+                  </Card.Footer>
+                </Card>
+              </AnimationOnScroll>
             </Col>
           ))}
         </Row>
