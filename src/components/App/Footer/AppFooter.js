f1f281cbe2e542f7e9f8652885b9f73c02ba87d4
Green Gem <chris_waitt@me.com>
2023-09-05 10:00:15 +0100

    Updated footer link colours and padding

--- src/components/App/Footer/AppFooter.js
+++ src/components/App/Footer/AppFooter.js
@@ -5,22 +5,24 @@ import { Gem } from 'react-bootstrap-icons';
 
 function AppFooter() {
   return (
-<Container fluid>
-  <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
-    <p className="col-md-4 mb-0 text-muted">© 2023 Chris Waitt</p>
+    <section id='footer'>
+      <Container fluid>
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3">
+          <p className="col-md-4 mb-0">© 2023 Chris Waitt</p>
 
-    <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-      <Gem />
-    </a>
+          <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto text-decoration-none">
+            <Gem />
+          </a>
 
-    <ul className="nav col-md-4 justify-content-end">
-      <li className="nav-item"><a href="/" className="nav-link px-2 text-muted">Home</a></li>
-      <li className="nav-item"><a href="/" className="nav-link px-2 text-muted">Github</a></li>
-      <li className="nav-item"><a href="/" className="nav-link px-2 text-muted">Linkedin</a></li>
-      <li className="nav-item"><a href="/" className="nav-link px-2 text-muted">Get in touch</a></li>
-    </ul>
-  </footer>
-</Container>
+          <ul className="nav col-md-4 justify-content-end">
+            <li className="nav-item"><a href="/" className="nav-link px-2">Home</a></li>
+            <li className="nav-item"><a href="/" className="nav-link px-2">Github</a></li>
+            <li className="nav-item"><a href="/" className="nav-link px-2">Linkedin</a></li>
+            <li className="nav-item"><a href="/" className="nav-link px-2">Get in touch</a></li>
+          </ul>
+        </footer>
+      </Container>
+    </section>
   );
 }
 
