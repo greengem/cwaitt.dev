8f22f7e8701f0d3be327c5429d8cef13ab4f69e7
Green Gem <chris_waitt@me.com>
2023-09-07 16:53:28 +0100

    feat: introductory text on project listing page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -20,6 +20,8 @@ export default function Projects({ projects }) {
     <section id='projects'>
       <Container className='mt-5'>
         <h1 className='section-h1'>My Projects</h1>
+        <p>Diving deep into the world of technology, I have always been captivated by the vast realm of possibilities that different tools and frameworks have to offer. For me, each project is more than just lines of code – it's an exploration, a challenge, and most importantly, a learning journey. From the tried-and-true classics to the latest shiny tech, I enjoy getting my hands dirty, always pushing the boundaries of what's possible.</p>
+        <p>Below, you'll find a curated collection of my endeavors, each with its unique tech flavor. Whether you're here to explore, get inspired, or just geek out, I hope you find these projects as exciting as I found creating them. Enjoy the journey!</p>
         <Row lg={2} md={2} sm={1} className='g-4'>
           {projects.map(project => (
             <ProjectCard project={project} key={project.sys.id} />
