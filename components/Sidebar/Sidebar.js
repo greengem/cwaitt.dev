1b0103bf6152c050acafb4752eed077d7d87f3cd
Green Gem <chris_waitt@me.com>
2023-09-07 10:41:04 +0100

    Migrade SidebarAboutMe Component

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -1,10 +1,11 @@
 import Link from 'next/link';
 import React from 'react';
+import SidebarAboutMe from './SidebarAboutMe/SidebarAboutMe';
 
 function AppSidebar() {
   return (
     <div className="sidebar">
-        
+        <SidebarAboutMe />
     </div>
   );
 }
---
+++ components/Sidebar/SidebarAboutMe/SidebarAboutMe.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import { Github, Linkedin } from 'react-bootstrap-icons';
+
+function SidebarAboutMe() {
+  return (
+    <Card className="mb-4">
+    <Card.Header>Author</Card.Header>
+      <Card.Body>
+        <Card.Title>Chris Waitt</Card.Title>
+        <Card.Text>I design and develop services for customers of all sizes, specializing in creating stylish, modern websites, web services and online stores.</Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <a href='https://www.linkedin.com/in/chriswaitt/' target="_blank" rel="noreferrer"><Linkedin className='me-2' /></a> <a href='https://github.com/greengem' target="_blank" rel="noreferrer"><Github /></a>
+      </Card.Footer>
+    </Card>
+  );
+}
+
+export default SidebarAboutMe;
