b5a81d4d6a6dce255a7b42c3e08862d9f6f53ef5
Green Gem <chris_waitt@me.com>
2023-09-07 08:52:46 +0100

    feat: Import Navbar and footer and display them sitewide

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@/styles/globals.css'
+import React from 'react';
+import AppNavbar from '../components/Navbar/Navbar';
+import AppFooter from '../components/Footer/Footer';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <AppNavbar />
+        <Component {...pageProps} />
+      <AppFooter />
+    </>
+  );
 }
+
+export default MyApp;
