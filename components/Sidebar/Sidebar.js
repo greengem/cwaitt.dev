83862ee630657bc43d08ff61fb7f339233086f54
Green Gem <chris_waitt@me.com>
2023-09-07 12:11:36 +0100

    issue: Migrate SidebarLatestPost
    Access token is not  being correctly passed to the component.
    Currently broken

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import SidebarAboutMe from './SidebarAboutMe/SidebarAboutMe';
 import SidebarGitHub from './SidebarGitHub/SidebarGitHub';
+import SidebarLatestPost from './SidebarLatestPost/SidebarLatestPost';
 
 function AppSidebar({ gitHubLink }) {
   return (
     <div className="sidebar">
         <SidebarAboutMe />
         <SidebarGitHub gitHubLink={gitHubLink} />
+        <SidebarLatestPost />
     </div>
   );
 }
