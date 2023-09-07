59e2367f7d7c837f91dbda2a3611d702e82586d4
Green Gem <chris_waitt@me.com>
2023-09-07 21:19:38 +0100

    fix: Nav dropdown now working

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -2,15 +2,16 @@ import Link from 'next/link';
 import React from 'react';
 import { Gem } from 'react-bootstrap-icons';
 
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
 function AppNavbar() {
   return (
-    <nav className="navbar bg-dark border-bottom border-body navbar-expand-lg" data-bs-theme="dark">
-        <div className='container'>
+    <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
             <Link href="/" className='navbar-brand'><Gem /></Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
                         <Link className='nav-link' href="/projects">Projects</Link>
@@ -19,9 +20,9 @@ function AppNavbar() {
                         <Link className='nav-link' href="/tech-stack">Tech</Link>
                     </li>
                 </ul>
-            </div>
-        </div>
-    </nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
   );
 }
 
