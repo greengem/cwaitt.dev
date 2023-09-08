38b5218cc17482c34ca97cfe31da912723d61f92
Green Gem <chris_waitt@me.com>
2023-09-08 21:39:25 +0100

    Migrated hero module style to globals

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
 
-import styles from './Hero.module.css'
-
 function AppHero() {
   return (
-    <section className={styles['hero-container']}>
-        <div className={styles['hero-overlay']}></div>
-        <div className={styles['hero-content']}>
+    <section className='hero-container'>
+        <div className='hero-overlay'></div>
+        <div className='hero-content'>
             <div className='container'>
-              <h1 className={styles['hero-content-h1']}>Hello, I&apos;m Chris Waitt</h1>
+              <h1 className='hero-content'>Hello, I&apos;m Chris Waitt</h1>
               <h2>Turning Ideas into Stunning Web Interfaces</h2>
               <p>Front-End Developer | UI/UX Enthusiast</p>
               <Link href="/projects" className='btn btn-dark'>View My Work</Link>
