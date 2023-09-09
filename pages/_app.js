50cd40391ab7af1c86ba1176ff683b0ad29c8244
Green Gem <chris_waitt@me.com>
2023-09-09 11:07:59 +0100

    Install and configure NextUI

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,11 @@
+import {NextUIProvider} from '@nextui-org/react'
+
+// Internal dependencies
+import '@/styles/globals.scss'; // Global styles for the application
+import React from 'react'; // React library (could be removed if using latest versions of Next.js since React import isn't mandatory)
+import AppNavbar from '../components/Navbar/Navbar'; // Navbar component
+import AppFooter from '../components/Footer/Footer'; // Footer component
+
 /**
  * Custom App component that wraps around all other pages. 
  * Use this component to keep layout or state consistent between pages.
@@ -10,9 +18,11 @@
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <AppNavbar /> {/* Site-wide navigation bar */}
-        <Component {...pageProps} /> {/* The main content of the page */}
-      <AppFooter /> {/* Site-wide footer */}
+      <NextUIProvider>
+        <AppNavbar /> {/* Site-wide navigation bar */}
+          <Component {...pageProps} /> {/* The main content of the page */}
+        <AppFooter /> {/* Site-wide footer */}
+      </NextUIProvider>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+import {nextui} from "@nextui-org/react";
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
  
     // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,5 +14,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
+
+export default config;
