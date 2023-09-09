087b257ec5364aabc6f0e0897190183de68214b4
Green Gem <chris_waitt@me.com>
2023-09-09 11:02:12 +0100

    Remove Bootstrap and install Tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,3 @@
-// External dependencies
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importing Bootstrap styles
-
-// Internal dependencies
-import '@/styles/globals.scss'; // Global styles for the application
-import React from 'react'; // React library (could be removed if using latest versions of Next.js since React import isn't mandatory)
-import AppNavbar from '../components/Navbar/Navbar'; // Navbar component
-import AppFooter from '../components/Footer/Footer'; // Footer component
-
 /**
  * Custom App component that wraps around all other pages. 
  * Use this component to keep layout or state consistent between pages.
