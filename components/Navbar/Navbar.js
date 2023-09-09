16cde1b02fe2e737dc29dd8565594896eab66b5f
Green Gem <chris_waitt@me.com>
2023-09-09 11:26:00 +0100

    Migrated Navbat to NextUI/TW

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,27 +1,33 @@
 import Link from 'next/link';
 import React from 'react';
+import { useRouter } from 'next/router';
+
+import {  Navbar,   NavbarBrand,   NavbarContent,   NavbarItem,   NavbarMenuToggle,  NavbarMenu,  NavbarMenuItem} from "@nextui-org/react";
 import { Gem } from 'react-bootstrap-icons';
 
 function AppNavbar() {
+    const router = useRouter();
   return (
-    <div className="navbar bg-body-tertiary navbar-expand-sm">
-        <div className='container'>
-            <Link href='/' className='navbar-brand'><Gem /></Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <Link className='nav-link' href="/projects">Projects</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className='nav-link' href="/tech-stack">Tech</Link>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
+    <Navbar>
+        <NavbarBrand><Gem className='me-1' /> <p className="font-bold text-inherit">CHRIS WAITT</p></NavbarBrand>
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem isActive={router.pathname === '/'}>
+            <Link href="/">
+                Home
+            </Link>
+        </NavbarItem>
+        <NavbarItem isActive={router.pathname.startsWith('/projects')}>
+            <Link href="/projects">
+                Projects
+            </Link>
+        </NavbarItem>
+        <NavbarItem isActive={router.pathname.startsWith('/tech-stack')}>
+            <Link href="/tech-stack">
+                Tech
+            </Link>
+        </NavbarItem>
+        </NavbarContent>
+    </Navbar>
   );
 }
 
