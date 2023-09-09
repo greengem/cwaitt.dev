a4a494dbdf7ea68ae7bcbc56d4f3013074b98c4d
Green Gem <chris_waitt@me.com>
2023-09-09 11:29:07 +0100

    fix: Corrected link syntax for next

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
-import Link from 'next/link';
+import {Link} from "@nextui-org/react";
+import NextLink from "next/link";
 import React from 'react';
 import { useRouter } from 'next/router';
 
@@ -12,17 +13,17 @@ function AppNavbar() {
         <NavbarBrand><Gem className='me-1' /> <p className="font-bold text-inherit">CHRIS WAITT</p></NavbarBrand>
         <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem isActive={router.pathname === '/'}>
-            <Link href="/">
+            <Link href="/" as={NextLink}>
                 Home
             </Link>
         </NavbarItem>
         <NavbarItem isActive={router.pathname.startsWith('/projects')}>
-            <Link href="/projects">
+            <Link href="/projects" as={NextLink}>
                 Projects
             </Link>
         </NavbarItem>
         <NavbarItem isActive={router.pathname.startsWith('/tech-stack')}>
-            <Link href="/tech-stack">
+            <Link href="/tech-stack" as={NextLink}>
                 Tech
             </Link>
         </NavbarItem>
