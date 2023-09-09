dd155520afe06b561d87bd6089ccfed0a5c80d78
Green Gem <chris_waitt@me.com>
2023-09-09 18:27:21 +0100

    Styled Navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -13,27 +13,28 @@ function AppNavbar({ isDarkMode, setIsDarkMode }) {
   const router = useRouter();
 
   return (
-    <Navbar>
+    <Navbar maxWidth="xl">
       <NavbarBrand><Gem className='me-1' /> <p className="font-bold text-inherit">CHRIS WAITT</p></NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem isActive={router.pathname === '/'}>
-          <Link href="/" as={NextLink}>
+          <Link href="/" as={NextLink} color="danger">
             Home
           </Link>
         </NavbarItem>
         <NavbarItem isActive={router.pathname.startsWith('/projects')}>
-          <Link href="/projects" as={NextLink}>
+          <Link href="/projects" as={NextLink} color="danger">
             Projects
           </Link>
         </NavbarItem>
         <NavbarItem isActive={router.pathname.startsWith('/tech-stack')}>
-          <Link href="/tech-stack" as={NextLink}>
+          <Link href="/tech-stack" as={NextLink} color="danger">
             Tech
           </Link>
         </NavbarItem>
         <Switch
+          size="sm"
           isSelected={isDarkMode}
-          color="secondary"
+          color="danger"
           startContent={<SunIcon />}
           endContent={<MoonIcon />}
           onValueChange={(isSelected) => setIsDarkMode(isSelected)}
