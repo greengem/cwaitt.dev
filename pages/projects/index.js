72ea756ea95e851e142f5f66bf8fe293e4ea5b5e
Green Gem <chris_waitt@me.com>
2023-09-09 18:28:57 +0100

    Migrated Projects index to Tailwind

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -15,10 +15,11 @@ export default function Projects({ projects }) {
   return (
     <section id='projects'>
       <div className='container mx-auto'>
-        <h1 className='section-h1'>My Projects</h1>
-        <p>Diving deep into the world of technology, I have always been captivated by the vast realm of possibilities that different tools and frameworks have to offer. For me, each project is more than just lines of code – it&apos;s an exploration, a challenge, and most importantly, a learning journey. From the tried-and-true classics to the latest shiny tech, I enjoy getting my hands dirty, always pushing the boundaries of what&apos;s possible.</p>
-        <p>Below, you&apos;ll find a curated collection of my endeavors, each with its unique tech flavor. Whether you&apos;re here to explore, get inspired, or just geek out, I hope you find these projects as exciting as I found creating them. Enjoy the journey!</p>
-
+        <h1 className='section-h1 mb-2 mt-0 text-5xl font-medium leading-tight'>My Projects</h1>
+        <div className='pb-10'>
+        <p className='pb-4'>Diving deep into the world of technology, I have always been captivated by the vast realm of possibilities that different tools and frameworks have to offer. For me, each project is more than just lines of code – it&apos;s an exploration, a challenge, and most importantly, a learning journey. From the tried-and-true classics to the latest shiny tech, I enjoy getting my hands dirty, always pushing the boundaries of what&apos;s possible.</p>
+        <p className='pb-4'>Below, you&apos;ll find a curated collection of my endeavors, each with its unique tech flavor. Whether you&apos;re here to explore, get inspired, or just geek out, I hope you find these projects as exciting as I found creating them. Enjoy the journey!</p>
+        </div>
         <div className='flex flex-wrap'>
           {projects.map(project => (
             <ProjectCard project={project} key={project.sys.id} />
