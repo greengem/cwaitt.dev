596eb4ba4574159e2fc2338d5374fa4cbb6b1c37
Green Gem <chris_waitt@me.com>
2023-09-09 18:27:01 +0100

    Migrated Hero to Tailwind

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {Link} from "@nextui-org/react";
-import NextLink from "next/link";
+import Link from 'next/link';
 import {Button, ButtonGroup} from "@nextui-org/react";
 
 function AppHero() {
@@ -12,7 +11,7 @@ function AppHero() {
               <h1 className='hero-content'>Hello, I&apos;m Chris Waitt</h1>
               <h2 className='mb-2 mt-0 text-4xl font-medium leading-tight'>Turning Ideas into Stunning Web Interfaces</h2>
               <p className='mb-4'>Front-End Developer | UI/UX Enthusiast</p>
-              <Link href="/projects"><Button className='bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg'>View My Work</Button></Link>
+              <Link href="/projects"><Button color='danger'>View My Work</Button></Link>
             </div>
         </div>
     </section>
