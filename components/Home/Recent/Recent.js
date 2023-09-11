c83abf315142690be2de989dac23a1865646b8ec
Green Gem <chris_waitt@me.com>
2023-09-11 15:35:22 +0100

    feat: Homepage recents now pulls the two latest posts from contentful.

--- components/Home/Recent/Recent.js
+++ components/Home/Recent/Recent.js
@@ -1,57 +1,48 @@
 import React from 'react';
-import {Image, Button, Link} from "@nextui-org/react";
+import { Image, Button, Link } from "@nextui-org/react";
 import NextLink from "next/link";
-import {Book} from 'react-bootstrap-icons';
+import { Book } from 'react-bootstrap-icons';
 
-function AppHomeRecent() {
-    return (
-<section id='recent' className='py-20'>
-    <div className='max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 mb-20 mt-10'>
+function AppHomeRecent({ projects }) {
+    
+  if (!projects || projects.length === 0) {
+    return <div>Loading...</div>;
+  }
+    
+  return (
+    <section id='recent' className='py-20'>
+      <div className='max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 mb-20 mt-10'>
         <h1 className='custom-heading from-[#FF705B] to-[#FFB457]'>My Recent Work</h1>
         
         <div className='flex flex-col md:flex-row space-y-20 md:space-y-0 md:space-x-20'>
-            <div className='w-full md:w-1/2'>
-                <div>
-                    <Image 
-                        isBlurred
-                        width={1920} 
-                        height={1080}
-                        alt="NextUI hero Image" 
-                        src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg" 
-                    />
-                </div>
-                <h4 className='text-xl font-semibold mt-10 mb-2'>Project</h4>
-                <p className='tracking-tight mb-6'>Lorem ipsum ...</p>
+          {projects.map(project => (
+            <div key={project.sys.id} className='w-full md:w-1/2'>
+                <Image 
+                  isBlurred
+                  width={1136}
+                  height={639}
+                  alt={`Image for ${project.fields.projectTitle}`}
+                  src={`https:${project.fields.featuredImage.fields.file.url}?fit=fill&w=1136&h=639`}
+                />
+              <NextLink href={`/projects/${project.fields.slug}`}>
+                  <h4 className='text-3xl tracking-tight font-semibold mt-10 mb-2 from-[#FF705B] to-[#FFB457] bg-clip-text text-transparent bg-gradient-to-b
+'>{project.fields.projectTitle}</h4>
+              </NextLink>
+              <p className='tracking-tight mb-6'>{project.fields.shortDescription}</p>
             </div>
-
-            <div className='w-full md:w-1/2'>
-                <div>
-                    <Image 
-                        isBlurred
-                        width={1920} 
-                        height={1080}
-                        alt="NextUI hero Image" 
-                        src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg" 
-                    />
-                </div>
-                <h4 className='text-xl font-semibold mt-10 mb-2'>Project</h4>
-                <p className='tracking-tight mb-6'>Lorem ipsum ...</p>
-            </div>
+          ))}
         </div>
 
-        <div className=' mt-10'>
-            <Link href='/projects' as={NextLink}>
-                <Button size='lg' className='bg-gradient-to-tr from-[#FF705B] to-[#FFB457]  text-white shadow-lg'>
-                    <Book />View My Work
-                </Button>
-            </Link>
+        <div className='mt-10'>
+          <Link href='/projects' as={NextLink}>
+            <Button size='lg' className='bg-gradient-to-tr from-[#FF705B] to-[#FFB457] text-white shadow-lg'>
+              <Book />View My Work
+            </Button>
+          </Link>
         </div>
-    </div>
-</section>
-
-
+      </div>
+    </section>
   );
 }
 
 export default AppHomeRecent;
-
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,34 @@
+// pages/index.js
+
 // Internal dependencies
 import AppHero from '../components/Home/Hero/Hero.js';
 import AppAbout from '../components/Home/About/About.js';
 import AppSkills from '@/components/Home/Skills/Skills.js';
-import AppHomeRecent from '@/components/Home/Recent/Recent.js';
+import AppHomeRecent from '@/components/Home/Recent/Recent.js'; // Ensure path is correct
 import AppSocial from '@/components/Home/Social/Social.js';
+import { fetchLatestProjects } from 'lib/contentful'; // Ensure path is correct
 
-export default function Home() {
+function Home({ projects }) {
   return (
     <>
       <AppHero />
       <AppAbout />
       <AppSkills />
-      <AppHomeRecent />
+      <AppHomeRecent projects={projects} />
       <AppSocial />
     </>
   );
 }
+
+export async function getStaticProps() {
+  const projects = await fetchLatestProjects(2);
+
+  return {
+    props: {
+      projects,
+    },
+    revalidate: 60, // Optional: Set how often Next.js should re-run this function (in seconds)
+  };
+}
+
+export default Home;
