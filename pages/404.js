a8d917f6bd4f24932f24a2c0c44ed7df6a8ee280
Green Gem <chris_waitt@me.com>
2023-09-11 17:59:13 +0100

    Updated and fixed 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,19 +1,28 @@
-// pages/404.js
-
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-//import styles from './404.module.css'  // If you want to add styles
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+//import { ExclamationCircle } from '@heroicons/react/solid';  // If you're using Heroicons. If not, you can add your own icon.
+import {ExclamationCircle} from 'react-bootstrap-icons';
 
 export default function Custom404() {
   const router = useRouter();
 
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The page <code>{router.asPath}</code> does not exist.</p>
-      <Link href="/">
-        Go back home
-      </Link>
-    </div>
-  )
+    <section id='projects' className='pt-20 min-h-screen flex flex-col justify-center'>
+      <div className='max-w-screen-xl mx-auto text-center'>
+        <div className='container mx-auto mb-20'>
+          <ExclamationCircle className="w-16 h-16 mx-auto text-red-500" />
+          <h1 className='custom-heading from-[#FF705B] to-[#FFB457] my-6'>Oops! Page Not Found</h1>
+          <div className='pb-5 space-y-6'>
+            <p className="text-3xl font-bold mb-4">404</p>
+            <p className="text-lg text-gray-700">We can't find the page you're looking for. It might have been moved or deleted.</p>
+            <div className="mt-8">
+              <Link className='bg-gradient-to-r from-[#FF705B] to-[#FFB457] text-white px-6 py-2 rounded-lg shadow-md hover:opacity-90' href="/">
+                  Return to Home
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
