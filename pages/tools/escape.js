5992476e808480a7d858f4930bec64b64837d2b9
Green Gem <chris_waitt@me.com>
2023-09-12 11:49:46 +0100

    Modified escape tool to ignore html tags, and added a clear text button

--- pages/tools/escape.js
+++ pages/tools/escape.js
@@ -1,18 +1,35 @@
 import { useState } from 'react';
-import {Textarea, Button, Code} from "@nextui-org/react";
+import { Textarea, Button, Code } from "@nextui-org/react";
+import { ArrowLeftRight} from 'react-bootstrap-icons';
 
 export default function EscapeTool() {
   const [inputText, setInputText] = useState('');
   const [outputText, setOutputText] = useState('');
 
+  // Function to handle the text escaping logic
   function handleEscape() {
-    const escapedText = inputText
-      .replace(/&/g, '&amp;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;')
-      .replace(/"/g, '&quot;')
-      .replace(/'/g, '&apos;');
+    // Define a function to escape text
+    const escapeChars = (str) => {
+        return str
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&apos;');
+    }
+
+    // Use regex to match text outside of tags, and apply the escape function to that text
+    const escapedText = inputText.replace(/(>)([^<]*)(<)/g, function(match, p1, p2, p3) {
+        return p1 + escapeChars(p2) + p3;
+    });
+
     setOutputText(escapedText);
+}
+
+  // Function to clear both input and output fields
+  function clearFields() {
+    setInputText('');
+    setOutputText('');
   }
 
   return (
@@ -20,42 +37,55 @@ export default function EscapeTool() {
       <div className='max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 mb-20 mt-10'>
         <div className="container mx-auto mt-10">
           <h1 className='custom-heading from-[#FF705B] to-[#FFB457]'>Text Escaper Tool</h1>
-          <p className='mb-10'>In web development, especially when dealing with HTML and JavaScript, certain characters can cause issues if not properly "escaped". These characters have special meanings in HTML and JS, and using them as-is can lead to unexpected results or even code vulnerabilities. Our Text Escaper Tool helps you seamlessly convert these problematic characters into their corresponding escape sequences, ensuring that your code remains clean and behaves as intended. The following symbols will be converted: <Code>&amp; to &amp;amp;</Code> <Code>&lt to &amp;lt;</Code> <Code>&gt; to &amp;gt;</Code> <Code>" to &amp;quot;</Code> <Code>' to &amp;apos;</Code> Simply input your text, and let the tool handle the rest!</p>
-            <div className='flex flex-col md:flex-row w-full'>
-              <div className='md:w-1/3'>
-                <Textarea 
-                  color='warning'
-                  minRows={17}
-                  maxRows={100}
-                  fullWidth
-                  label="Input"
-                  placeholder="Enter your text here..."
-                  value={inputText}
-                  onChange={(e) => setInputText(e.target.value)}>
-                </Textarea>
-              </div>
-              <div className="md:w-1/3 text-center my-10">
-              <Button className='mx-5 bg-gradient-to-tr from-[#FF705B] to-[#FFB457] text-black shadow-lg' size='lg' onClick={handleEscape}>Escape Text</Button>
-            </div>
-              <div className='md:w-1/3'>
-                <Textarea
-                  color='warning'
-                  minRows={17}
-                  maxRows={100}
-                  fullWidth
-                  label="Output"
-                  variant="bordered"
-                  isReadOnly
-                  value={outputText}>
-                </Textarea>
-              </div>
-            </div>
-
-          </div>
+          <p className='mb-10'>
+            In web development, especially when dealing with HTML and JavaScript, certain characters can cause issues if not properly "escaped". These characters have special meanings in HTML and JS, and using them as-is can lead to unexpected results or even code vulnerabilities. Our Text Escaper Tool helps you seamlessly convert these problematic characters into their corresponding escape sequences, ensuring that your code remains clean and behaves as intended. The following symbols will be converted: 
+            <Code>&amp; to &amp;amp;</Code> 
+            <Code>&lt; to &amp;lt;</Code> 
+            <Code>&gt; to &amp;gt;</Code> 
+            <Code>" to &amp;quot;</Code> 
+            <Code>' to &amp;apos;</Code> 
+            Simply input your text, and let the tool handle the rest!
+          </p>
           
+          <div className='flex flex-col md:flex-row w-full'>
+            {/* Input field */}
+            <div className='md:w-1/3'>
+              <Textarea 
+                color='warning'
+                minRows={17}
+                maxRows={100}
+                fullWidth
+                label="Input"
+                placeholder="Enter your text here..."
+                value={inputText}
+                onChange={(e) => setInputText(e.target.value)}
+              />
+            </div>
+            
+            {/* Button controls */}
+            <div className="md:w-1/3 text-center my-10 flex flex-col items-center">
+    <Button className='mx-5 mb-4 bg-gradient-to-tr from-[#FF705B] to-[#FFB457] text-black shadow-lg' size='lg' onClick={handleEscape}><ArrowLeftRight />Escape Text</Button>
+    <Button color='default' className='mx-5' size='lg' onClick={clearFields}>Clear</Button>
+</div>
+
+
+
+            {/* Output field */}
+            <div className='md:w-1/3'>
+              <Textarea
+                color='warning'
+                minRows={17}
+                maxRows={100}
+                fullWidth
+                label="Output"
+                variant="bordered"
+                isReadOnly
+                value={outputText}
+              />
+            </div>
+          </div>
         </div>
-      </section>
+      </div>
+    </section>
   );
 }
-
-
