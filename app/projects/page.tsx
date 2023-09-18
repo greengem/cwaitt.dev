7345a05affb3c613bd439b5dd81adce9dab5ffaf
Green Gem <chris_waitt@me.com>
2023-09-18 13:30:20 +0100

    Removed the homeage fetch and prop since everything is done within the recent component. Refactored projects page too

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -12,7 +12,7 @@ interface ProjectProps {
   slug: string;
   projectTitle: string;
   shortDescription: string;
-  featuredImageUrl: string;
+  featuredImageUrl: any;
 }
 function Project({
   slug,
@@ -27,17 +27,17 @@ function Project({
         <h4 className="font-bold text-large whitespace-nowrap overflow-hidden overflow-ellipsis w-full">{projectTitle}</h4>
     </CardHeader>
     <CardBody className="overflow-visible py-2">
-        <Link as={NextLink} href={`/projects/${slug}`}>
-                <Image
-                    shadow="lg"
-                    as={NextImage}
-                    src={`${featuredImageUrl}?fit=fill&w=1136&h=639`}
-                    className='object-cover rounded-xl'
-                    width={1268}
-                    height={951}
-                />
-        </Link>
-        <p className="card-text mt-5 text-sm">{shortDescription}</p>
+      <Link as={NextLink} href={`/projects/${slug}`}>
+        <Image
+            shadow="lg"
+            as={NextImage}
+            src={`${featuredImageUrl}?fit=fill&w=1136&h=639`}
+            className='object-cover rounded-xl'
+            width={1268}
+            height={951}
+        />
+      </Link>
+      <p className="card-text mt-5 text-sm">{shortDescription}</p>
     </CardBody>
     {/*
     <CardFooter className='px-5'>
@@ -63,7 +63,6 @@ function Project({
 export default async function Page() {
   const { isEnabled } = draftMode();
   const allProjects = await getAllProjects(isEnabled);
-  console.log("All projects with description:", allProjects);
   return (
     <section id='projects' className='pt-20'>
       <div className='max-w-screen-xl mx-auto'>
@@ -71,19 +70,16 @@ export default async function Page() {
           <h1 className='custom-heading from-[#FF705B] to-[#FFB457]'>My Projects</h1>
             <div className='pb-5'>
               <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10'>
-                
-
-      {allProjects.map((project, index) => (
-  <Project
-    key={index}
-    slug={project.slug}
-    projectTitle={project.projectTitle}
-    shortDescription={project.shortDescription}
-    featuredImageUrl={project.featuredImage.url}
-  />
-))}
-
-</div>
+                {allProjects.map((project, index) => (
+                  <Project
+                    key={index}
+                    slug={project.slug}
+                    projectTitle={project.projectTitle}
+                    shortDescription={project.shortDescription}
+                    featuredImageUrl={project.featuredImage.url}
+                  />
+                ))}
+              </div>
             </div>
         </div>
       </div>
