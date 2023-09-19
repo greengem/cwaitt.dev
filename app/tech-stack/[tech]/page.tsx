c138f2df06049731971e9f253eff7bc106095900
Green Gem <chris_waitt@me.com>
2023-09-19 18:03:20 +0100

    temporarily ignoring the tech stack singles until i can figure out how to query it properly.

--- app/tech-stack/[tech]/page.tsx
+++ app/tech-stack/[tech]/page.tsx
@@ -1,37 +1,96 @@
-import { getAllTechStacks, getProjectsByTechStack } from '../../../lib/api';
+import { draftMode } from 'next/headers';
+import { getAllProjects } from '../../../lib/api';
+import NextLink from 'next/link';
+import NextImage from 'next/image';
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+} from '@nextui-org/card';
+import { Chip } from '@nextui-org/chip';
+import { Image } from '@nextui-org/image';
+import { Link } from '@nextui-org/link';
 
-function TechStackProjectsPage({ techName, projects }) {
+interface ProjectProps {
+  slug: string;
+  projectTitle: string;
+  shortDescription: string;
+  featuredImageUrl: any;
+  techStacks: { slug: string; name: string }[];
+}
+
+function Project({
+  slug,
+  projectTitle,
+  shortDescription,
+  featuredImageUrl,
+  techStacks,
+}: ProjectProps) {
   return (
-    <section id='tech' className='pt-20'>
-      <div className='max-w-screen-xl mx-auto'>
-        <div className='container mx-auto min-h-screen'>
-          <h1 className='custom-heading from-[#FF72E1] to-[#F54C7A]'>
-            Projects using {techName}
+    <Card className="py-4 h-full" shadow="lg">
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+        <h4 className="font-bold text-large whitespace-nowrap overflow-hidden overflow-ellipsis w-full">
+          {projectTitle}
+        </h4>
+      </CardHeader>
+      <CardBody className="overflow-visible py-2">
+        <Link as={NextLink} href={`/projects/${slug}`}>
+          <Image
+            shadow="lg"
+            as={NextImage}
+            src={`${featuredImageUrl}?fit=fill&w=1136&h=639`}
+            className="object-cover rounded-xl"
+            width={1268}
+            height={951}
+          />
+        </Link>
+        <p className="card-text mt-5 text-sm">{shortDescription}</p>
+      </CardBody>
+      <CardFooter className="px-5">
+        {techStacks.map((stack) => (
+          <Chip
+            key={stack.slug}
+            variant="bordered"
+            color="warning"
+            className="mr-1"
+          >
+            <Link color='warning' as={NextLink} href={`/tech-stack/${stack.slug}`}>{stack.name}</Link>
+          </Chip>
+        ))}
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default async function Page() {
+  const { isEnabled } = draftMode();
+  const allProjects = await getAllProjects(isEnabled);
+
+  return (
+    <section id="projects" className="pt-20">
+      <div className="max-w-screen-xl mx-auto">
+        <div className="container mx-auto mb-20">
+          <h1 className="custom-heading from-[#FF705B] to-[#FFB457]">
+            My Projects
           </h1>
-          <ul className="list-disc pl-5">
-            {projects.map(project => (
-              <li key={project.slug}>
-                <strong>{project.projectTitle}</strong>
-                <p>{project.shortDescription}</p>
-              </li>
+          <div className="pb-5">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10">
+            {allProjects.map((project) => (
+              <Project
+                key={project.slug}  // Here's the change
+                slug={project.slug}
+                projectTitle={project.projectTitle}
+                shortDescription={project.shortDescription}
+                featuredImageUrl={project.featuredImage.url}
+                techStacks={project.techStacksCollection.items}
+              />
             ))}
-          </ul>
+
+            </div>
+          </div>
         </div>
       </div>
     </section>
   );
 }
-
-// Assuming you will write the getProjectsByTechStack function to get the list of projects
-export async function fetchData(techSlug) {
-  const projects = await getProjectsByTechStack(techSlug);
-  const allTechStacks = await getAllTechStacks(false); // replace false with appropriate draft mode flag if needed
-  const techStack = allTechStacks.find(tech => tech.slug === techSlug);
-  
-  return {
-    techName: techStack ? techStack.name : "Unknown Tech",
-    projects,
-  };
-}
-
-export default TechStackProjectsPage;
