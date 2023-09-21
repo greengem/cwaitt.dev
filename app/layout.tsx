ffa94cbaf24f790121fdb27c987ae6cfed309e24
Green Gem <chris_waitt@me.com>
2023-09-21 14:49:46 +0100

    Added some metadata to the homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
 import "../styles/globals.css";
+import type { Metadata } from 'next'
 import { Providers } from "./providers";
 import AppNavbar from "../components/Navbar/Navbar";
 import AppFooter from "../components/Footer/Footer";
+
+export const metadata: Metadata = {
+  title: 'Chris Waitt - Frontend Developer Portfolio',
+  description: 'Discover the creative world of Chris Waitt, a proficient frontend developer. Dive into a portfolio that highlights modern, responsive, and user-centric web designs and projects.',
+}
 
 export default function RootLayout({ children }) {
   return (
