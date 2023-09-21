5224ac38f771fff78d53d8534882a1f96c4e576f
Green Gem <chris_waitt@me.com>
2023-09-21 10:59:51 +0100

    Attempted to implement draft mode in next and revalidations with vercel. currently not working

---
+++ app/api/draft/route.ts
@@ -0,0 +1,22 @@
+import { draftMode } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { getPreviewProjectBySlug } from '../../../lib/api'
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const secret = searchParams.get('secret')
+  const slug = searchParams.get('slug')
+
+  if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET) {
+    return new Response('Invalid token', { status: 401 })
+  }
+
+  const project = await getPreviewProjectBySlug(slug)
+
+  if (!project) {
+    return new Response('Invalid slug', { status: 401 })
+  }
+
+  draftMode().enable()
+  redirect(`/projects/${project.slug}`)
+}
