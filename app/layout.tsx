2dbb17a52106f3d7748571aa19907ef431022be7
Green Gem <chris_waitt@me.com>
2023-09-22 11:02:08 +0100

    Added Sass and decided to use highlight.js instead of react-syntax-highlighter so i can render code snippets on the server instead of client

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "../styles/globals.scss";
 import type { Metadata } from 'next'
 import { Providers } from "./providers";
 import AppNavbar from "../components/Navbar/Navbar";
