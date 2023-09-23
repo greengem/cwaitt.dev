631a3c75dbe8710d25804f09ac67cd9d8df52ee7
Green Gem <chris_waitt@me.com>
2023-09-23 13:30:39 +0100

    ui: redesigned hero (in progress)

--- components/Home/Hero/Hero.js
+++ components/Home/Hero/Hero.js
@@ -1,97 +1,53 @@
-'use client'
-import React, { useEffect, useRef } from 'react';
-import Typed from 'typed.js';
-import { motion } from 'framer-motion';
 import NextLink from "next/link";
 import { Button } from "@nextui-org/button";
 import { Link } from "@nextui-org/link";
-import { IconBrandGithub } from '@tabler/icons-react';
+import { Divider } from "@nextui-org/divider";
+import { IconBrandGithub, IconBrandReact, IconBrandNextjs, IconBrandVue, IconBrandAngular, IconBrandTailwind, IconBrandPython, IconBrandPhp, IconBrandBootstrap, IconBrandThreejs} from '@tabler/icons-react';
 
 export default function AppHero() {
-  const elRef = useRef(null);
-
-  useEffect(() => {
-    const options = {
-      strings: [
-        `Hello,^1000 I'm <span class="from-[#FF1CF7] to-[#b249f8] bg-clip-text text-transparent bg-gradient-to-b">Chris Waitt</span>`
-      ],
-      typeSpeed: 50,
-      backSpeed: 50,
-      loop: false,
-      contentType: 'html',
-      onComplete: (self) => {
-        if (self.cursor) {
-          self.cursor.style.animation = 'none';
-          self.cursor.style.opacity = 0;
-        }
-      }
-    };
-
-    if (elRef.current) {
-      const typed = new Typed(elRef.current, options);
-
-      return () => {
-        typed.destroy();
-      };
-    }
-  }, []);
-
-  const fadeInH2 = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { delay: 3, duration: 2 } }
-};
-
-const fadeInP = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { delay: 4, duration: 2 } }
-};
-
-const fadeInButtons = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { delay: 5, duration: 2 } }
-};
-
   return (
-    <section id='hero' className='hero-container'>
-      <div className='hero-overlay'></div>
-      <div className='hero-content'>
-        <div className='container text-center mb-5'>
-          <h1 className='tracking-tight font-semibold text-6xl md:text-7xl lg:text-8xl mb-5'>
-            <span ref={elRef}></span>
-          </h1>
-          <motion.h2 
-                variants={fadeInH2}
-                initial="hidden"
-                animate="visible"
-                className='tracking-tight font-semibold text-2xl md:text-3xl mb-1'>
-                Turning Ideas into Stunning Web Interfaces
-            </motion.h2>
-            <motion.p 
-                variants={fadeInP}
-                initial="hidden"
-                animate="visible"
-                className='text-lg md:text-xl tracking-tight mb-6'>
-                Front-End Developer | UI/UX Enthusiast
-            </motion.p>
-            <motion.div 
-            className='flex justify-center items-center gap-x-2'
-            variants={fadeInButtons}
-            initial="hidden"
-            animate="visible"
-        >
-            <Link as={NextLink} href="/projects">
-                <Button size="lg" className='bg-gradient-to-tr from-[#FF1CF7] to-[#b249f8]  text-white shadow-lg'>
-                    View My Work
-                </Button>
-            </Link>
-            <Link as={NextLink} href="https://github.com/greengem" isExternal>
-                <Button color='default' size="lg" variant="bordered">
-                    <IconBrandGithub />Github
-                </Button>
-            </Link>
-        </motion.div>
-        </div>
+<section id='hero'>
+  <div className="max-w-screen-xl mx-auto h-screen">
+    <div className="container h-full text-center">
+      <p className="notice mt-20 mb-20">
+        This website is currently <strong>under construction</strong>
+      </p>
+      <h1 className='tracking-tight font-bold text-6xl md:text-7xl lg:text-8xl mb-10'>
+        Hello, I'm <span className="from-[#FF1CF7] to-[#b249f8] bg-clip-text text-transparent bg-gradient-to-b">Chris Waitt</span>
+      </h1>
+      <h2 className="text-2xl md:text-3xl mb-1">
+        Turning Ideas into Stunning Web Interfaces
+      </h2>
+      <p className="text-lg md:text-xl mb-20">
+        Front-End Developer | UI/UX Enthusiast
+      </p>
+      <div className="flex justify-center items-center w-full mb-20">
+        <Link as={NextLink} href="/projects" className="mr-3">
+          <Button color="default" size="lg">
+            View My Work
+          </Button>
+        </Link>
+        <Link as={NextLink} href="https://github.com/greengem" isExternal>
+          <Button color='default' size="lg" variant="bordered">
+            <IconBrandGithub /> Github
+          </Button>
+        </Link>
       </div>
-    </section>
+      <div className="grid gap-y-7 grid-cols-5 max-w-screen-md mx-auto justify-items-center">
+        <IconBrandReact className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={48} height={48} />
+        <IconBrandNextjs className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={48} height={48} />
+        <IconBrandVue className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={48} height={48} />
+        <IconBrandAngular className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={48} height={48} />
+        <IconBrandTailwind className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={48} height={48} />
+        <IconBrandThreejs className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={48} height={48} />
+        <IconBrandBootstrap className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={48} height={48} />
+        <IconBrandPython className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={48} height={48} />
+        <IconBrandPhp className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={48} height={48} />
+        <IconBrandGithub className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={48} height={48} />
+      </div>
+    </div>
+  </div>
+</section>
+
   );
 }
