036ceb749d003859901a4460ef1de93286e94da8
Green Gem <chris_waitt@me.com>
2023-09-23 13:28:54 +0100

    fix: markdown was exluded from tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
-import {nextui} from "@nextui-org/react";
+import { nextui } from '@nextui-org/react';
 
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    './lib/markdown.tsx'
   ],
   theme: {
     container: {
@@ -15,7 +15,7 @@ const config = {
     },
     extend: {},
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [nextui({
     themes: {
       light: {},
