9ece28f3d7f64983f49c5c48f6e7a39a4b7a5e9c
Green Gem <chris_waitt@me.com>
2023-09-23 19:52:23 +0100

    fix: removed star.js until i do more testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="overflow-x-hidden">
+      <body>
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <AppNavbar />
               {children}
--- components/Home/Hero/Hero_star_backup.js
+++ components/Home/Hero/Hero_star_backup.js
@@ -1,15 +1,45 @@
 'use client'
+import { useTheme } from "next-themes";
+import { useEffect, useState } from 'react';
+import { motion } from "framer-motion";
 import NextLink from "next/link";
 import { Button } from "@nextui-org/button";
 import { Link } from "@nextui-org/link";
 import { IconBrandGithub, IconBrandReact, IconBrandNextjs, IconBrandVue, IconBrandAngular, IconBrandTailwind, IconBrandPython, IconBrandPhp, IconBrandBootstrap, IconBrandThreejs} from '@tabler/icons-react';
+import Star from "./Star";
 
 export default function AppHero() {
+  const numStars = 100;
+  const { theme } = useTheme();
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  const renderStars = () => {
+    if (theme === 'dark' && hasMounted) {
+      return Array.from({ length: numStars }).map((_, i) => (
+        <motion.div
+          key={i}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5, delay: Math.random() * 1.5 }}
+        >
+          <Star delay={Math.random()} />
+        </motion.div>
+      ));
+    }
+    return null;
+  };
+
   return (
-    <section id='hero' className="pt-20 h-screen">
+    <section id='hero' className="pt-40 h-screen overflow-x-hidden">
+        {renderStars()}
+
       <div className="max-w-screen-xl mx-auto">
         <div className="container h-full text-center">
-          <p className="notice mb-3 text-xs">
+          <p className="notice mb-3">
             <strong>Notice:</strong> This website is currently <strong>under construction</strong>
           </p>
           <h1 className='tracking-tight font-bold text-6xl md:text-7xl lg:text-8xl mb-10'>
@@ -18,7 +48,7 @@ export default function AppHero() {
           <h2 className="text-2xl md:text-3xl mb-1">
             Turning Ideas into Stunning Web Interfaces
           </h2>
-          <p className="text-lg md:text-xl mb-10">
+          <p className="text-lg md:text-xl mb-20">
             Front-End Developer | UI/UX Enthusiast
           </p>
           <div className="mb-20">
@@ -28,20 +58,19 @@ export default function AppHero() {
               </Button>
             </Link>
           </div>
-          <p className="text-xs uppercase mb-4">- All the latest tech -</p>
-          <div className="grid gap-y-6 grid-cols-5 max-w-screen-sm mx-auto justify-items-center mb-20">
+          <p className="text-xs uppercase mb-2">- All the latest tech -</p>
+          <div className="grid gap-y-7 grid-cols-5 max-w-screen-sm mx-auto justify-items-center">
             <IconBrandReact className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={32} height={32} />
             <IconBrandNextjs className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={32} height={32} />
             <IconBrandVue className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={32} height={32} />
             <IconBrandAngular className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={32} height={32} />
             <IconBrandTailwind className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={32} height={32} />
-            {/* <IconBrandThreejs className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={32} height={32} />
+            <IconBrandThreejs className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={32} height={32} />
             <IconBrandBootstrap className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={32} height={32} />
             <IconBrandPython className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={32} height={32} />
             <IconBrandPhp className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={32} height={32} />
             <IconBrandGithub className="transition ease-in-out duration-300 hover:text-secondary" stroke={1} width={32} height={32} />
-  */}
-            </div>
+          </div>
         </div>
       </div>
     </section>
