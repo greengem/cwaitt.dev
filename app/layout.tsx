d0b6924fc97c900c5593bf7904c0ef8130eceea3
Green Gem <chris_waitt@me.com>
2023-09-24 19:10:32 +0100

    decided that having extended live previews are not worth making the component client rendered. simple previews are enough for now

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import type { Metadata } from 'next'
 import { Providers } from "./providers";
 import AppNavbar from "../components/Navbar/Navbar";
 import AppFooter from "../components/Footer/Footer";
-import { ContentfulLivePreviewProvider } from '@contentful/live-preview/react';
-
 export const metadata: Metadata = {
   title: 'Chris Waitt - Frontend Developer Portfolio',
   description: 'Discover the creative world of Chris Waitt, a proficient frontend developer. Dive into a portfolio that highlights modern, responsive, and user-centric web designs and projects.',
@@ -15,16 +13,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-        <ContentfulLivePreviewProvider
-  locale="set-your-locale-here" // Required: allows you to set the locale once and have it reused throughout the preview.
-  enableInspectorMode={true} // Optional: allows you to toggle inspector mode which is on by default.
-  enableLiveUpdates={true} // Optional: allows you to toggle live updates which are on by default.
-  debugMode={true} // Optional: allows you to toggle debug mode which is off by default.
->
           <AppNavbar />
               {children}
           <AppFooter />
-          </ContentfulLivePreviewProvider>
         </Providers>
       </body>
     </html>
