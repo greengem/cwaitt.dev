d43fac693aba356e35eb097949b1d0492d348879
Green Gem <chris_waitt@me.com>
2023-09-24 19:08:29 +0100

    testing extended live preview features

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Providers } from "./providers";
 import AppNavbar from "../components/Navbar/Navbar";
 import AppFooter from "../components/Footer/Footer";
+import { ContentfulLivePreviewProvider } from '@contentful/live-preview/react';
 
 export const metadata: Metadata = {
   title: 'Chris Waitt - Frontend Developer Portfolio',
@@ -14,9 +15,16 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+        <ContentfulLivePreviewProvider
+  locale="set-your-locale-here" // Required: allows you to set the locale once and have it reused throughout the preview.
+  enableInspectorMode={true} // Optional: allows you to toggle inspector mode which is on by default.
+  enableLiveUpdates={true} // Optional: allows you to toggle live updates which are on by default.
+  debugMode={true} // Optional: allows you to toggle debug mode which is off by default.
+>
           <AppNavbar />
               {children}
           <AppFooter />
+          </ContentfulLivePreviewProvider>
         </Providers>
       </body>
     </html>
