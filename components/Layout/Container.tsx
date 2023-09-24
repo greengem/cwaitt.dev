40c71d8c19eddfbb07456b8a48ce301b42c23123
Green Gem <chris_waitt@me.com>
2023-09-24 11:22:56 +0100

    fix: Export with no name and export with duplicate name

--- components/Layout/Container.tsx
+++ components/Layout/Container.tsx
@@ -1,4 +1,4 @@
-export default function({children}) {
+export default function Container({children}) {
     return (
         <div className="max-w-screen-xl mx-auto px-4 md:px-8">
             {children}
--- components/Layout/Section/PageSection.tsx
+++ components/Layout/Section/PageSection.tsx
@@ -1,4 +1,4 @@
-export default function HomeSection({ children, id = 'default-section' }) {
+export default function PageSection({ children, id = 'default-section' }) {
     return (
         <section id={id} className="pt-20">
             {children}
