15d0a2f7865138b032d62e4a7d1cf4d1e5559e0a
Green Gem <chris_waitt@me.com>
2023-11-22 18:30:40 +0000

    Split projects into two sections to focus on finished projects. added a cool animated card that links to my github repos in fitting with the work in progress projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,5 @@
 import { draftMode } from 'next/headers';
-import { getAllProjects } from '@/lib/api';
+import { getAllProjectsWip, getWorkInProgress } from '@/lib/api';
 import PageSection from '@/components/Layout/Section/PageSection'
 import Container from '@/components/Layout/Container'
 import Heading from '@/components/Layout/Heading/Heading';
@@ -9,28 +9,46 @@ import GitHubCard from '@/components/Cards/GitHubCard';
 
 export default async function ProjectsPage() {
   const { isEnabled } = draftMode();
-  const allProjects: ProjectProps[] = await getAllProjects(isEnabled);
+  const allProjects: ProjectProps[] = await getAllProjectsWip(isEnabled);
+  const wipProjects: ProjectProps[] = await getWorkInProgress(isEnabled);
 
   return (
     <PageSection id='projects'>
       <Container>
         <Heading title="My Projects" />
-          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
-            {allProjects.map((project) => (
-              <ProjectCard
-                key={project.slug}
-                slug={project.slug}
-                projectTitle={project.projectTitle}
-                shortDescription={project.shortDescription}
-                featuredImage={project.featuredImage}
-                techStacksCollection={project.techStacksCollection}
-                projectTags={project.projectTags}
-                isFeatured={project.isFeatured}
-              />
-            ))}
-            <GitHubCard />
-          </div>
-        </Container>
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
+          {allProjects.map((project) => (
+            <ProjectCard
+              key={project.slug}
+              slug={project.slug}
+              projectTitle={project.projectTitle}
+              shortDescription={project.shortDescription}
+              featuredImage={project.featuredImage}
+              techStacksCollection={project.techStacksCollection}
+              projectTags={project.projectTags}
+              isFeatured={project.isFeatured}
+            />
+          ))}
+        </div>
+
+        <Heading title="Work In Progress" className='mt-20' />
+
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
+          <GitHubCard />
+          {wipProjects.map((project) => (
+            <ProjectCard
+              key={project.slug}
+              slug={project.slug}
+              projectTitle={project.projectTitle}
+              shortDescription={project.shortDescription}
+              featuredImage={project.featuredImage}
+              techStacksCollection={project.techStacksCollection}
+              projectTags={project.projectTags}
+              isFeatured={project.isFeatured}
+            />
+          ))}
+        </div>
+      </Container>
     </PageSection>
   );
 }
