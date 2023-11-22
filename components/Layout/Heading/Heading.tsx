1f22e5b0efcbb627160a5a04de0de654ab7b30a5
Green Gem <chris_waitt@me.com>
2023-11-22 18:28:22 +0000

    Allow heading to accept className prop

--- components/Layout/Heading/Heading.tsx
+++ components/Layout/Heading/Heading.tsx
@@ -1,6 +1,6 @@
-export default function Heading({ title }) {
+export default function Heading({ title, className = '' }) {
     return (
-        <h1 className='
+        <h1 className={`
             tracking-tight 
             font-semibold 
             text-4xl 
@@ -9,7 +9,8 @@ export default function Heading({ title }) {
             mb-5 
             md:mb-10
             lg:mb-10
-        '>
+            ${className}
+        `}>
             <span className="bg-clip-text text-transparent bg-gradient-to-b from-[#FF1CF7] to-[#b249f8]">
                 {title}
             </span>
