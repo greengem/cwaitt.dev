4c5137602a43c728754e0960fdc40a65a153d4ff
Green Gem <chris_waitt@me.com>
2023-11-23 11:16:07 +0000

    Add Skill certs to skills section

--- components/Home/Skills/Skills.tsx
+++ components/Home/Skills/Skills.tsx
@@ -3,6 +3,7 @@ import Container from '@/components/Layout/Container';
 import Heading from '@/components/Layout/Heading/Heading';
 import SkillsCard from './SkillsCard';
 import SkillsData from './SkillsData';
+import SkillsCerts from './SkillsCerts';
 
 export default function AppSkills() {
   return (
@@ -10,11 +11,12 @@ export default function AppSkills() {
       <div className='skills-overlay'></div>
       <Container>
       <Heading title="Technical Expertise" />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5 mb-5">
           {SkillsData.map((skill, index) => (
             <SkillsCard key={index} {...skill} />
           ))}
         </div>
+        <SkillsCerts />
       </Container>
     </HomeSection>
   );
