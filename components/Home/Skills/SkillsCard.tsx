b20df67ae9cc805deb7d7e17c4974411e3e17c42
Green Gem <chris_waitt@me.com>
2023-11-23 11:11:14 +0000

    Updated skill cards to match max width

--- components/Home/Skills/SkillsCard.tsx
+++ components/Home/Skills/SkillsCard.tsx
@@ -14,13 +14,13 @@ type SkillsCardProps = {
 
 const SkillCard: React.FC<SkillsCardProps> = ({ title, icon, description, technologies, animation }) => {
   return (
-    <AnimationOnScroll animateIn={animation}>
-    <Card shadow="none" className="p-4 shadow-lg">
-      <CardHeader className="text-xl">
+    <AnimationOnScroll animateIn={animation} className='h-full' animateOnce>
+    <Card shadow="none" className="shadow-lg h-full">
+      <CardHeader className="text-xl px-5 pt-5 pb-0">
         {icon} {title}
       </CardHeader>
-      <CardBody className="text-default-700 tracking-tight card-body relative">
-        <p>{description}</p>
+      <CardBody className="text-default-700 tracking-tight relative px-5 pb-5">
+        <p className='text-sm text-default-600'>{description}</p>
         <div className="flex flex-wrap mt-6 gap-1">
           {technologies.map((tech: string, index: number) => (
             <Chip size="sm" key={index} color="secondary" variant='bordered'>
