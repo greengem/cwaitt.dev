db27966b8cf8ef5896c65db22c7b74da3e9b9f0a
Green Gem <chris_waitt@me.com>
2023-11-23 11:30:53 +0000

    Removed redundant code from theme switcher after removing DOM manipulation

--- components/Helpers/DarkModeToggle/DarkModeToggle.tsx
+++ components/Helpers/DarkModeToggle/DarkModeToggle.tsx
@@ -1,47 +1,38 @@
-import { useState, useEffect } from "react";
-import { useTheme } from "next-themes";
+"use client";
+import React from "react";
 import { Switch } from "@nextui-org/react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import { SunIcon } from "@/components/Icons/SunIcon";
 import { MoonIcon } from "@/components/Icons/MoonIcon";
-
-export const ThemeSwitcher = () => {
-  const { theme, setTheme } = useTheme();
+export function ThemeSwitcher() {
   const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
   useEffect(() => {
     setMounted(true);
-    const initialThemeColor = theme === "dark" ? "#000000" : "#ffffff";
-    document.querySelector('meta[name="theme-color"]')?.setAttribute('content', initialThemeColor);
-  }, [theme]);
+  }, []);
 
-  useEffect(() => {
-    if (theme === 'dark') {
-      import('highlight.js/styles/github-dark.css');
-    } else {
-      import('highlight.js/styles/github.css');
-    }
-  }, [theme]);
+  if (!mounted) return null;
 
-  if (!mounted || theme === undefined) return null;
+  const isDarkMode = theme === "dark";
 
-  return (
-    <div>
+  return (    
       <Switch
-        name="themeSwitch"
-        id=":r0:"
+        isSelected={isDarkMode}
         size="md"
-        className="ms-2"
         color="secondary"
-        startContent={<SunIcon />}
-        endContent={<MoonIcon />}
-        isSelected={theme === "dark"}
-        onValueChange={(isSelected) => {
-          const newTheme = isSelected ? "dark" : "light";
-          setTheme(newTheme);
-          //const themeColor = newTheme === "dark" ? "#18181b" : "#ffffff";
-          //document.querySelector('meta[name="theme-color"]')?.setAttribute('content', themeColor);
+        thumbIcon={({ isSelected, className }) =>
+          isSelected ? (
+            <MoonIcon className={className} />
+          ) : (
+            <SunIcon className={className} />
+          )
+        }
+        onChange={() => {
+          setTheme(isDarkMode ? "light" : "dark");
         }}
-      />
-    </div>
+      >
+      </Switch>
   );
-};
+}
