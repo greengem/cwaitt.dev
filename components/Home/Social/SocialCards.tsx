c8c1b483e757d6db68f9dd37dbf359e89424aada
Green Gem <chris_waitt@me.com>
2023-11-23 11:16:21 +0000

    Animate contact cards on scroll

--- components/Home/Social/SocialCards.tsx
+++ components/Home/Social/SocialCards.tsx
@@ -1,13 +1,16 @@
+'use client'
 import { Card, CardHeader } from "@nextui-org/card";
 import { Link } from "@nextui-org/link";
 import NextLink from "next/link";
 import SocialData from '@/components/Home/Social/SocialData';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 const SocialCards: React.FC = () => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
             {SocialData.map((card, index) => (
-                <Card key={index} shadow="none" className="shadow-lg">
+                <AnimationOnScroll key={index} animateIn="animate__zoomIn" className='h-full' animateOnce>
+                <Card shadow="none" className="shadow-lg">
                     <CardHeader className="flex gap-3">
                         <card.icon stroke={1} height="40" width="40" />
                         <div className="flex flex-col">
@@ -21,6 +24,7 @@ const SocialCards: React.FC = () => {
                         </div>
                     </CardHeader>
                 </Card>
+                </AnimationOnScroll>
             ))}
         </div>
     );
