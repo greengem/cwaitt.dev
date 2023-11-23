b0929b083c54d98c56ef4a1646112356baf615c8
Green Gem <chris_waitt@me.com>
2023-11-23 11:10:45 +0000

    Added scroll animation to featured posts

--- components/Home/Recent/RecentPosts.tsx
+++ components/Home/Recent/RecentPosts.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { ProjectProps } from '@/types/appTypes';
 import ProjectCard from '@/components/Cards/ProjectCard';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 type RecentPostsProps = {
   twoProjects: ProjectProps[];
@@ -9,16 +11,18 @@ const RecentPosts: React.FC<RecentPostsProps> = ({ twoProjects }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 gap-5">
       {twoProjects.map((project: ProjectProps, index: number) => (
-        <ProjectCard
-          key={index}
-          slug={project.slug}
-          projectTitle={project.projectTitle}
-          shortDescription={project.shortDescription}
-          featuredImage={project.featuredImage}
-          techStacksCollection={project.techStacksCollection}
-          projectTags={project.projectTags}
-          isFeatured={project.isFeatured}
-        />
+        <AnimationOnScroll key={project.slug} animateIn="animate__zoomIn" className='h-full' animateOnce>
+          <ProjectCard
+            key={index}
+            slug={project.slug}
+            projectTitle={project.projectTitle}
+            shortDescription={project.shortDescription}
+            featuredImage={project.featuredImage}
+            techStacksCollection={project.techStacksCollection}
+            projectTags={project.projectTags}
+            isFeatured={project.isFeatured}
+          />
+        </AnimationOnScroll>
       ))}
     </div>
   );
