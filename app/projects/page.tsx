c48332aa8245486cc5613f9f3169d82e991bbce1
Green Gem <chris_waitt@me.com>
2023-12-04 16:32:21 +0000

    Added descriptive text underneath the title of the work in progress project listing

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -32,8 +32,10 @@ export default async function ProjectsPage() {
           ))}
         </div>
 
-        <Heading title="Work In Progress" className='mt-20' />
+        <Heading title="Work In Progress" className='mt-20 mb-2 md:mb-2 lg:mb-2' />
 
+        <p className='text-default-500 mb-7 md:mb-10'>The projects listed below are currently under active development. While they showcase my ongoing work and evolving skills, please note that they may contain bugs and some features might still be in the implementation phase. Each project comes with a GitHub link for you to explore the code, and where available, a live preview link. I welcome any feedback or contributions to these projects as they continue to grow and improve!</p>  
+        
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
           <GitHubCard />
           {wipProjects.map((project) => (
