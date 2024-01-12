fa1646306b05e1fb044b56efe0478ba751cd5832
Green Gem <chris_waitt@me.com>
2024-01-12 15:41:43 +0000

    Updated home projects CTA

--- components/Home/Recent/RecentCta.tsx
+++ components/Home/Recent/RecentCta.tsx
@@ -1,11 +1,12 @@
 import { Button } from '@nextui-org/button';
+import { IconCode } from '@tabler/icons-react';
 import Link from 'next/link';
 
 export default function RecentHeading() {
     return (
         <div className="mt-10">
-            <Button href="/projects" as={Link} size="lg" className="bg-gradient-to-tr from-[#FF1CF7] to-[#b249f8] text-white shadow-lg">
-                View All My Work
+            <Button href="/projects" as={Link} size="lg" className="bg-gradient-to-tr from-[#FF1CF7] to-[#b249f8] text-white shadow-lg  gap-unit-1">
+                <IconCode /> More Projects
             </Button>
         </div>
     );
