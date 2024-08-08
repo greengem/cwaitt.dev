061ba3eec10daf444bff68c228eeea09eb49a415
Green Gem <chris_waitt@me.com>
2024-08-08 20:22:34 +0100

    chore: Add CopyButton component for copying code to clipboard

---
+++ components/Code/CopyButton.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React, { useState } from "react";
+import copy from "copy-to-clipboard";
+import { IconCopy, IconCheck } from "@tabler/icons-react";
+import { Tooltip } from "@nextui-org/tooltip";
+import { toast } from "sonner";
+
+export const CopyButton = ({ code }) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopy = () => {
+    copy(code);
+    toast.success("Code copied to clipboard.");
+    setIsCopied(true);
+    setTimeout(() => setIsCopied(false), 3000);
+  };
+
+  return (
+    <Tooltip content="Copy">
+      <button
+        onClick={handleCopy}
+        aria-label="Copy to clipboard"
+        disabled={isCopied}
+      >
+        {isCopied ? <IconCheck size={16} /> : <IconCopy size={16} />}
+      </button>
+    </Tooltip>
+  );
+};
