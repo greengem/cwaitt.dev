0b158c5258da62529e5d33ecc4e4274b5fca6d22
Green Gem <chris_waitt@me.com>
2024-08-08 20:22:39 +0100

    chore: Update emailjs import and add toast library for notifications

--- components/Home/Social/ContactForm.tsx
+++ components/Home/Social/ContactForm.tsx
@@ -1,12 +1,22 @@
-'use client'
-import React, { useRef, FormEvent } from 'react';
-import emailjs from '@emailjs/browser';
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Input, Textarea} from "@nextui-org/react";
-import toast from 'react-hot-toast';
-import { IconMailFilled } from '@tabler/icons-react';
+"use client";
+import React, { useRef, FormEvent } from "react";
+import emailjs from "@emailjs/browser";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  Input,
+  Textarea,
+} from "@nextui-org/react";
+import { toast } from "sonner";
+import { IconMailFilled } from "@tabler/icons-react";
 
 export const ContactForm = () => {
-  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const form = useRef<HTMLFormElement>(null);
 
   const sendEmail = (e: FormEvent) => {
@@ -17,18 +27,24 @@ export const ContactForm = () => {
     const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY as string;
 
     if (form.current) {
-      emailjs.sendForm(serviceID, templateID, form.current, publicKey)
-        .then((result) => {
-          toast.success('Email successfully sent!');
-        }, (error) => {
-          toast.error('Failed to send email. Please try again.');
-        });
+      emailjs.sendForm(serviceID, templateID, form.current, publicKey).then(
+        (result) => {
+          toast.success("Email successfully sent!");
+        },
+        (error) => {
+          toast.error("Failed to send email. Please try again.");
+        }
+      );
     }
   };
 
   return (
     <>
-      <Button size='lg' onPress={onOpen} className='mb-5 gap-unit-1 bg-gradient-to-tr from-[#FF1CF7] to-[#b249f8] text-white shadow-lg'>
+      <Button
+        size="lg"
+        onPress={onOpen}
+        className="mb-5 gap-unit-1 bg-gradient-to-tr from-[#FF1CF7] to-[#b249f8] text-white shadow-lg"
+      >
         <IconMailFilled />
         Contact Form
       </Button>
@@ -38,24 +54,49 @@ export const ContactForm = () => {
           {(onClose) => (
             <>
               <form ref={form} onSubmit={sendEmail}>
-                <ModalHeader className="flex flex-col gap-1">Contact Form</ModalHeader>
+                <ModalHeader className="flex flex-col gap-1">
+                  Contact Form
+                </ModalHeader>
                 <ModalBody>
-                    <Input type="text" name="user_name" label="Name" placeholder="Joey Blogs" isRequired />
-                    <Input type="email" name="user_email" label="Email" placeholder="name@example.com" isRequired />
-                    <Textarea label="Message" placeholder="Enter your message" name="message" isRequired />
+                  <Input
+                    type="text"
+                    name="user_name"
+                    label="Name"
+                    placeholder="Joey Blogs"
+                    isRequired
+                  />
+                  <Input
+                    type="email"
+                    name="user_email"
+                    label="Email"
+                    placeholder="name@example.com"
+                    isRequired
+                  />
+                  <Textarea
+                    label="Message"
+                    placeholder="Enter your message"
+                    name="message"
+                    isRequired
+                  />
                 </ModalBody>
                 <ModalFooter>
                   <Button color="danger" variant="light" onPress={onClose}>
                     Cancel
                   </Button>
-                  <Button color='primary' type="submit" onPress={onClose} value="Send">Send</Button>
+                  <Button
+                    color="primary"
+                    type="submit"
+                    onPress={onClose}
+                    value="Send"
+                  >
+                    Send
+                  </Button>
                 </ModalFooter>
               </form>
             </>
           )}
         </ModalContent>
       </Modal>
-
     </>
   );
 };
